2c42caad18bc76f638ee4c79a609d0097cfb21ef
Julien Goletto <julien.goletto@gmail.com>
2023-06-01 10:20:23 +0200

    :sparkles: Configuration is over
    graphql endpoint is live, first types are casted, Query class for resolution is done. Everything works.

---
+++ cookbook/schema.py
@@ -0,0 +1,28 @@
+import graphene
+from graphene_django import DjangoObjectType
+
+from ingredients.models import Category, Ingredient
+
+class CategoryType(DjangoObjectType):
+    class Meta:
+        model = Category
+        fields = ("id", "name", "ingredients")
+
+class IngredientType(DjangoObjectType):
+    class Meta:
+        model = Ingredient
+        fields = ("id", "name", "notes", "category")
+
+class Query(graphene.ObjectType):
+    all_ingredients = graphene.List(IngredientType)
+    all_categories = graphene.List(CategoryType)
+
+    def resolve_all_ingredients(root, info):
+        return Ingredient.objects.select_related("category").all()
+    def resolve_all_categories(root, info, name):
+        try:
+            return Category.objects.get(name=name)
+        except Category.DoesNotExist:
+            return None
+
+schema = graphene.Schema(query=Query)
